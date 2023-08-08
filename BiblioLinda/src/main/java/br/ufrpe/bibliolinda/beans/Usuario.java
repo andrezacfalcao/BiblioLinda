1e871c223bd62b173fa8bd5f917c21a22c60be2c
Andreza Falcao <129098892+andrezacfalcao@users.noreply.github.com>
2023-08-08 20:07:02 -0300

    Get e set da classe Usuario

--- BiblioLinda/src/main/java/br/ufrpe/bibliolinda/beans/Usuario.java
+++ BiblioLinda/src/main/java/br/ufrpe/bibliolinda/beans/Usuario.java
@@ -12,4 +12,36 @@ public abstract class Usuario {
         this.senha = senha;
         this.tipo = tipo;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
 }
