81e817377f2b106d2b27c972ff1e939d905a1aa1
Andreza Falcao <129098892+andrezacfalcao@users.noreply.github.com>
2023-08-08 20:32:29 -0300

    Get e set da classe Livro

--- BiblioLinda/src/main/java/br/ufrpe/bibliolinda/beans/Livro.java
+++ BiblioLinda/src/main/java/br/ufrpe/bibliolinda/beans/Livro.java
@@ -17,4 +17,32 @@ public class Livro {
         this.anoDeLancamento = anoDeLancamento;
         this.numeroDeCopias = numeroDeCopias;
     }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+    public void setNomeLivro (String nomeLivro){
+        this.nomeLivro = nomeLivro;
+    }
+    public Categoria getCategoriaLivro(){
+        return categoriaLivro;
+    }
+
+    public void setCategoriaLivro(Categoria categoriaLivro) {
+        this.categoriaLivro = categoriaLivro;
+    }
+
+    public LocalDate getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(LocalDate anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+    public int getNumeroDeCopias(){
+        return numeroDeCopias;
+    }
+    public void setNumeroDeCopias(int numeroDeCopias){
+        this.numeroDeCopias = numeroDeCopias;
+    }
 }
